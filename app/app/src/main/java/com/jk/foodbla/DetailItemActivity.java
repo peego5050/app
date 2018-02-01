ee3f187a0064eb67703425c9ff3f4670265e529c
kuratlij <kuratlij@student.ethz.ch>
2018-02-01 16:19:37 +0100

    Added files

---
+++ app/app/src/main/java/com/jk/foodbla/DetailItemActivity.java
@@ -0,0 +1,38 @@
+package com.jk.foodbla;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class DetailItemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_item);
+
+        ArrayList<Item> items = new ArrayList<Item>();
+        items.add(new Item("Item 1", "4.75"));
+        items.add(new Item("Item 2", "5.25"));
+        ArrayAdapter<Item> a = new ArrayAdapter<Item>(this, android.R.layout.simple_list_item_1, items);
+
+        ListView l = (ListView)findViewById(R.id.listViewDetail);
+        l.setAdapter(a);
+        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Return selected item
+                Intent result = new Intent();
+                result.putExtra("position", position);
+                setResult(RESULT_OK, result);
+                finish();
+            }
+        });
+    }
+}
