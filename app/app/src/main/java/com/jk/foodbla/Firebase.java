5ae5f54602ced7e6bb92e3c238ad4e3063e4da18
mac <pirmin.real@gmail.com>
2018-02-09 11:50:29 +0100

    layout & tests
    png drawable added
    
    icon button added
    
    constraint layout basics
    
    app theme tests (fullscreen, sticky immersion)
    
    scrollview

--- app/app/src/main/java/com/jk/foodbla/Firebase.java
+++ app/app/src/main/java/com/jk/foodbla/Firebase.java
@@ -19,6 +19,17 @@ public class Firebase extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_firebase);
 
+
+        //fullscreen modus 'sticky immersion'
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+
         // Open connection to DB
         FirebaseDatabase db = FirebaseDatabase.getInstance();
         DatabaseReference dbRef = db.getReference("count");
--- app/app/src/main/java/com/jk/foodbla/settingsActivity.java
+++ app/app/src/main/java/com/jk/foodbla/settingsActivity.java
@@ -1,13 +1,28 @@
 package com.jk.foodbla;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class settingsActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+        //fullscreen modus 'sticky immersion'
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
 }
